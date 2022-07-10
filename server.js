29a441271f679d14748309a33bb4f2eb3ec72826
trongduong98 <duongnguyen13092013@gmail.com.com>
2022-07-10 10:20:57 +0700

    revert

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+
 const app = express();
- 
-const forceSSL = function () {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-};
-app.use(express.static('./dist/my-bio-app'));
- 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname,'/dist/my-bio-app/index.html'));
+
+app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
- 
-app.use(forceSSL());
- 
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 4200);
