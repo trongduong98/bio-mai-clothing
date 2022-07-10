450d6cb533648ef229fd02f34ac99e4d85ef480b
trongduong98 <duongnguyen13092013@gmail.com.com>
2022-07-10 10:31:46 +0700

    revert

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/my-bio-app'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/my-bio-app/index.html'));
 });
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
