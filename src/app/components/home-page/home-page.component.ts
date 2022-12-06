a4a64489b7b97a59a8e836c75bb72ffbb2b47edf
trongduong98 <duongnguyen13092013@gmail.com.com>
2022-12-06 21:02:28 +0700

    change logo

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -28,9 +28,6 @@ export class HomePageComponent implements OnInit {
       case 'zalo':
         window.open("https://zalo.me/g/vlqefv859");
         break;
-      case 'vay':
-        window.open("https://shopee.vn/V%C3%A1y-Jean-ng%E1%BA%AFn-33cm-l%C3%B3t-qu%E1%BA%A7n-n%E1%BB%8Bt-khoen-l%C6%B0ng-ch%E1%BA%A5t-co-d%C3%A3n-i.33371895.14786911099?sp_atk=d29a356c-e45b-4b9c-ae24-ff7b1ee63487&xptdk=d29a356c-e45b-4b9c-ae24-ff7b1ee63487");
-        break;
       default:
         break;
     }
