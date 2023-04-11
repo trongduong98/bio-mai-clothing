bbe18d6f1310a1ac08938766fedcc1967466ec47
trongduong98 <duongnguyen13092013@gmail.com>
2023-04-11 15:39:58 +0700

    Update home-page.component.ts

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -27,7 +27,7 @@ export class HomePageComponent implements OnInit {
         window.open("https://www.tiktok.com/@maiclothing1");
         break;
       case 'zalo':
-        window.open("https://zalo.me/g/vlqefv859","_blank");
+        window.location.replace("https://zalo.me/g/vlqefv859");
         break;
       default:
         break;
